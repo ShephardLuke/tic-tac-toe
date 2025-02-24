39e65048cc4d2cccd1a7123fabcbcf9daf40eb94
shephardluke <shephardluke@hotmail.com>
2025-02-24 13:54:55 +0000

    Adjust header responsive design
    Signed-off-by: shephardluke <shephardluke@hotmail.com>

--- app/template/global/header.tsx
+++ app/template/global/header.tsx
@@ -26,11 +26,11 @@ export default function Header({currentPage}: {currentPage?: string}) {
     })
 
     return (
-        <div className="flex flex-col text-center md:flex-row justify p-10 header">
+        <div className="flex flex-col text-center lg:flex-row justify p-10 header">
             <div className="header whitespace-nowrap">
                 <Link className="text-4xl" href={"/"}>{MAIN_TITLE}</Link>
             </div>
-            <div key={"links"} className="flex flex-col pt-5 space-y-5 md:space-y-0 md:pt-0 md:flex-row justify-around header text-2xl items-center w-full">
+            <div key={"links"} className="flex flex-col pt-5 space-y-5 lg:space-y-0 lg:pt-0 lg:flex-row justify-around header text-2xl items-center w-full">
                 {pageLinks}
             </div>
         </div>
